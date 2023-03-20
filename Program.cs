0779f4f910436501b915b67efc7b22b86b229ab4
unknown <zab_vl@tutby>
2023-03-20 22:27:22 +0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число");
+string num = Console.ReadLine();
+bool palindrome = true;
+
+for (int i=0; i<num.Length/2;i++)
+{
+    if(num[i]==num[num.Length-1-i]) palindrome=true;
+    else 
+    {
+        palindrome=false;
+        break;
+    }
+}
+
+if (palindrome==true) Console.WriteLine("Палиндром!");
+else Console.WriteLine("Не палиндром!");
+
